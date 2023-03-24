776b51581ce8e76daeb57ef0f55aa3b82f1ae9f7
Aditya Phasu <adityaphasu919@gmail.com>
2023-03-24 19:57:48 +0530

    add and config tooltips

--- src/components/Chart.jsx
+++ src/components/Chart.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import Data from "../data.json";
-import { Chart as ChartJS, BarElement, LinearScale, CategoryScale, Title } from "chart.js";
+import { Chart as ChartJS, BarElement, LinearScale, CategoryScale, Title, Tooltip } from "chart.js";
 import { Bar } from "react-chartjs-2";
 
-ChartJS.register(BarElement, LinearScale, CategoryScale, Title);
+ChartJS.register(BarElement, LinearScale, CategoryScale, Title, Tooltip);
 ChartJS.defaults.font.family = "DM Sans";
 
 const day = Data.map((x) => x.day);
@@ -29,6 +29,26 @@ function Chart() {
   const options = {
     responsive: true,
     maintainAspectRatio: false,
+    plugins: {
+      tooltip: {
+        enabled: true,
+        displayColors: false,
+        xAlign: "center",
+        yAlign: "bottom",
+        position: "average",
+        caretPadding: 5,
+        caretSize: 0,
+        callbacks: {
+          title: () => {
+            return "";
+          },
+
+          label: (context) => {
+            return "$" + context.raw;
+          },
+        },
+      },
+    },
     layout: {
       padding: {
         left: -10,
